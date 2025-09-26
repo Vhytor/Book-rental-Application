aa7a5e59d727badee948a82045c0aee38c1983e9
Vhytor <ikechukwuchukwunyerem@gmail.com>
2025-09-26 04:19:52 -0700

    Updating creation of to be handled by only admin

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const authMiddleWare = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    if(!authHeader) return res.status(401).json({error: "No authorization header"});
+
+
     const token = req.headers.authorization?.split(" ")[1];
     if(!token) return res.status(401).json({error: "No token provided"});
 
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { createBook, getBooks, getBookById, updateBook, deleteBook } from "../controller/book.controller.js";
+import * as bookController from "../controller/book.controller.js";
 import { authMiddleWare, isAdmin } from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
 
-router.post("/", createBook);
+router.post("/", authMiddleWare,isAdmin, (req,res,next) => {
+    if(req.user.role != "admin"){
+        return res.status(403).json({error: "Only admin can create books"});
+    }
+    return bookController.createBook(req,res,next);
+
+});
 router.get("/", getBooks);
 router.get("/:id", getBookById);
 router.put("/:id", updateBook);
