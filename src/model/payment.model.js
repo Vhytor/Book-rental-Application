89c3f66541af3aea1d05fe68d393bd6129fde5fb
Vhytor <ikechukwuchukwunyerem@gmail.com>
2025-10-08 05:42:13 -0700

    new features

--- src/model/payment.model.js
+++ src/model/payment.model.js
@@ -4,5 +4,15 @@ import mongoose from "mongoose";
 
 
 const paymentSchema = new mongoose.Schema({
-    user: {type:mongoose.Schema.Types.ObjectId, ref:"User", required: true}
-});
+    user: {type:mongoose.Schema.Types.ObjectId, ref:"User", required: true},
+    rental: {type:mongoose.Schema.Types.ObjectId, ref:"Rental", required: true},
+    amount: {type: Number, required: true},
+    status: {type: String, enum:["pending", "success", "failed"], default: "pending"},
+    reference: {type: String, unique: true},
+    paymentDate: {type: Date, default: Date.now},
+
+
+}, {timestamps: true});
+
+
+export default mongoose.model("Payment", paymentSchema);
